9a13f547099dd4b1282d494f6ef6c11787184aaa
sanketh2003 <109782077+sanketh2003@users.noreply.github.com>
2024-06-05 06:04:41 +0000

    1 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          This is the Event <span className="heart">♥️😊</span> React
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
