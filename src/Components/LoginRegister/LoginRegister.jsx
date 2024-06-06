6c4cc834e091a3a9219df17435e35408c08cfdaf
sanketh2003 <109782077+sanketh2003@users.noreply.github.com>
2024-06-06 05:06:10 +0000

    4th commit

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -47,32 +47,39 @@ const LoginRegister = () => {
         <div className='form-box register'>
             <form action=" ">
                 <h1>Registration</h1>
+
                 <div className="input-box">
                     <input type="text" 
                     placeholder='Username' 
                     required/>
                     <FaUser className='icon'/>
                 </div>
+
                 <div className="input-box">
                     <input type="email" 
                     placeholder='Email' 
                     required/>
                     <MdEmail  className='icon'/>
                 </div>
+
                 <div className="input-box">
                     <input type="password" 
                     placeholder='Password' 
                     required/>
                     <FaLock className='icon'/>
                 </div>
+
                 <div className="remember-forgot">
                     <label><input type="checkbox" />I agree to the term & conditions</label>
                        
                 </div>
+
                 <button type="submit">Register</button>
+
                 <div className="register-link">
                     <p>Don't have an account?<a href='#' onClick={loginLink}>Login</a></p>
                 </div>
+
             </form>
         </div>
     </div>
